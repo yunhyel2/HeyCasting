4951220537bc17bb04459ed805ce1f0414495e4f
HyeRi <yunhyel2@naver.com>
2017-03-16 15:15:04 +0900

    style

--- resources/views/Join/create.blade.php
+++ resources/views/Join/create.blade.php
@@ -2,20 +2,36 @@
 
 @section('content')
     <div class="step">
+        <ul>
         @if( Request::segment(1) == 'singer' )
-            <p>프로필 등록하기 > 가수</p>
+            <li>프로필 등록하기 <i class="fa fa-angle-right" aria-hidden="true"></i> <a href="#">가수 <i class="fa fa-angle-down" aria-hidden="true"></i></a>
         @elseif( Request::segment(1) == 'actor' )
-            <p>프로필 등록하기 > 배우</p>
+            <li>프로필 등록하기 <i class="fa fa-angle-right" aria-hidden="true"></i> <a href="#">배우 <i class="fa fa-angle-down" aria-hidden="true"></i></a>
         @elseif( Request::segment(1) == 'musician' )
-            <p>프로필 등록하기 > 연주자</p>
+            <li>프로필 등록하기 <i class="fa fa-angle-right" aria-hidden="true"></i> <a href="#">연주자 <i class="fa fa-angle-down" aria-hidden="true"></i></a>
         @elseif( Request::segment(1) == 'dancer' )
-            <p>프로필 등록하기 > 댄서</p>
+            <li>프로필 등록하기 <i class="fa fa-angle-right" aria-hidden="true"></i> <a href="#">댄서 <i class="fa fa-angle-down" aria-hidden="true"></i></a>
         @elseif( Request::segment(1) == 'host' )
-            <p>프로필 등록하기 > MC, 사회자</p>
+            <li>프로필 등록하기 <i class="fa fa-angle-right" aria-hidden="true"></i> <a href="#">MC, 사회자 <i class="fa fa-angle-down" aria-hidden="true"></i></a>
         @elseif( Request::segment(1) == 'model' )
-            <p>프로필 등록하기 > 모델</p>
+            <li>프로필 등록하기 <i class="fa fa-angle-right" aria-hidden="true"></i> <a href="#">모델 <i class="fa fa-angle-down" aria-hidden="true"></i></a>
         @endif
+            <ul class="select">
+                <li class="{{ Request::segment(1) == 'singer' ? 'hidden' : '' }}"><a href="{{ url('singer/create') }}">가수</a></li>
+                <li class="{{ Request::segment(1) == 'actor' ? 'hidden' : '' }}"><a href="{{ url('actor/create') }}">배우</a></li>
+                <li class="{{ Request::segment(1) == 'player' ? 'hidden' : '' }}"><a href="{{ url('player/create') }}">연주자</a></li>
+                <li class="{{ Request::segment(1) == 'dancer' ? 'hidden' : '' }}"><a href="{{ url('dancer/create') }}">댄서</a></li>
+                <li class="{{ Request::segment(1) == 'host' ? 'hidden' : '' }}"><a href="{{ url('host/create') }}">MC, 사회자</a></li>
+                <li class="{{ Request::segment(1) == 'model' ? 'hidden' : '' }}"><a href="{{ url('model/create') }}">모델</a></li>
+            </ul>
+        </li>
+        </ul>
     </div>
+    <script>
+        $('div.step > ul > li > a').on('click', function(){
+            $('ul.select').toggleClass('active-show');
+        });
+    </script>
     <div class="page join-form">
         <form method="post" ENCTYPE="multipart/form-data" action="{{ url('join') }}">
         {{ csrf_field() }}
@@ -40,9 +56,58 @@
                     <label for="password-confirm">비밀번호확인</label>
                     <input type="password" value="" id="password-confirm" name="password-confirm" placeholder="비밀번호를 다시 한번 입력해주세요."/>
                 </div>
-                <div class="form-group">
+                <div class="form-group half">
                     <label for="phone">핸드폰</label>
                     <input type="text" value="" id="phone" name="phone" placeholder="숫자만 입력해주세요."/>
+                </div>
+                <div class="form-group half">
+                    <label for="address">주소</label>
+                    <input type="text" id="sample6_postcode" placeholder="우편번호">
+                    <input type="button" onclick="sample6_execDaumPostcode()" value="우편번호찾기"><br>
+                    <input type="text" name="address" id="sample6_address" placeholder="주소">
+                    <!--- 주소검색 -->
+                        <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
+                        <script>
+                            function sample6_execDaumPostcode() {
+                                new daum.Postcode({
+                                    oncomplete: function(data) {
+                                        // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+                                        // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+                                        // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                                        var fullAddr = ''; // 최종 주소 변수
+                                        var extraAddr = ''; // 조합형 주소 변수
+
+                                        // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+                                        if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                                            fullAddr = data.roadAddress;
+
+                                        } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                                            fullAddr = data.jibunAddress;
+                                        }
+
+                                        // 사용자가 선택한 주소가 도로명 타입일때 조합한다.
+                                        if(data.userSelectedType === 'R'){
+                                            //법정동명이 있을 경우 추가한다.
+                                            if(data.bname !== ''){
+                                                extraAddr += data.bname;
+                                            }
+                                            // 건물명이 있을 경우 추가한다.
+                                            if(data.buildingName !== ''){
+                                                extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                                            }
+                                            // 조합형주소의 유무에 따라 양쪽에 괄호를 추가하여 최종 주소를 만든다.
+                                            fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
+                                        }
+
+                                        // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                                        document.getElementById('sample6_postcode').value = data.zonecode; //5자리 새우편번호 사용
+                                        document.getElementById('sample6_address').value = fullAddr;
+                                    }
+                                }).open();
+                            }
+                        </script>
+                    <!--- end 주소검색 -->
                 </div>
             </div>
             <div class="group team-group">
@@ -171,55 +236,6 @@
                 </select>
             </div>
             <div class="form-group half">
-                <label for="address">주소</label>
-                <input type="text" id="sample6_postcode" placeholder="우편번호">
-                <input type="button" onclick="sample6_execDaumPostcode()" value="우편번호찾기"><br>
-                <input type="text" name="address" id="sample6_address" placeholder="주소">
-                <!--- 주소검색 -->
-                    <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
-                    <script>
-                        function sample6_execDaumPostcode() {
-                            new daum.Postcode({
-                                oncomplete: function(data) {
-                                    // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-
-                                    // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-                                    // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-                                    var fullAddr = ''; // 최종 주소 변수
-                                    var extraAddr = ''; // 조합형 주소 변수
-
-                                    // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-                                    if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
-                                        fullAddr = data.roadAddress;
-
-                                    } else { // 사용자가 지번 주소를 선택했을 경우(J)
-                                        fullAddr = data.jibunAddress;
-                                    }
-
-                                    // 사용자가 선택한 주소가 도로명 타입일때 조합한다.
-                                    if(data.userSelectedType === 'R'){
-                                        //법정동명이 있을 경우 추가한다.
-                                        if(data.bname !== ''){
-                                            extraAddr += data.bname;
-                                        }
-                                        // 건물명이 있을 경우 추가한다.
-                                        if(data.buildingName !== ''){
-                                            extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-                                        }
-                                        // 조합형주소의 유무에 따라 양쪽에 괄호를 추가하여 최종 주소를 만든다.
-                                        fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
-                                    }
-
-                                    // 우편번호와 주소 정보를 해당 필드에 넣는다.
-                                    document.getElementById('sample6_postcode').value = data.zonecode; //5자리 새우편번호 사용
-                                    document.getElementById('sample6_address').value = fullAddr;
-                                }
-                            }).open();
-                        }
-                    </script>
-                <!--- end 주소검색 -->
-            </div>
-            <div class="form-group half">
                 <label for="perform_minutes">평균공연시간</label>
                 <input type="text" value="" id="perform_minutes" name="perform_minutes" placeholder="분 단위로 입력해주세요. ( 예 : 30 - 40 )"/>
             </div>
@@ -231,7 +247,7 @@
                     <input type="checkbox" id="cost_flag" name="cost_flag" value="N"/>
                 </div>
             </div>
-            <div class="form-group">
+            <div class="form-group half">
                 <label for="video">활동 Youtube 영상</label>
                 <input type="text" value="" id="video" name="video" placeholder="Youtube 영상 URL을 입력해주세요."/>
             </div>
