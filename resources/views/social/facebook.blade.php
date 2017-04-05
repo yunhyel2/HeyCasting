fb0a131fff00a3e9bf841857accb27794fd7976d
dajeong <lillian2.3lee@gmail.com>
2017-04-05 13:27:51 +0900

    merge

--- resources/views/social/facebook.blade.php
+++ resources/views/social/facebook.blade.php
@@ -23,7 +23,7 @@
         'into this app.';
     }
   }
-
+sdfsdf
   // This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
   // code below.
