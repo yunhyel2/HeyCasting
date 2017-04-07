865e28c5a87dd3af1f3347c1ef500b123584bc0c
HyeRi <yunhyel2@naver.com>
2017-04-07 17:04:01 +0900

    modify

--- resources/views/Join/complete.blade.php
+++ resources/views/Join/complete.blade.php
@@ -7,7 +7,7 @@
                 <h2 class="logo"><img src="/img/logo2.png" alt="헤이캐스팅"/></h2>
                 <p>{{ $user }} 회원 가입이 완료되었습니다</p>
                 <a href="{{ url('/') }}" class="button">메인으로 가기</a>
-                <a href="{{ $user == '엔터테이너' ? '/enter-join' : '/user-join' }}" class="button">{{ $user }} 회원 가입하기</a>
+                <a href="{{ $user == '엔터테이너' ? '/user-join' : '/enter-join' }}" class="button">{{ $user == '엔터테이너' ? '일반' : '엔터테이너' }} 회원 가입하기</a>
             </div>
         </form>
     </div>
